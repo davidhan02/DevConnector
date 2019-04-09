23aa292632f241a04ffeb36c7d13553fdf95ab9c
David Han <davidhan.5.25@gmail.com>
2019-04-08 21:12:07 -0700

    Added validation handlers for email and password, completed validation handlers

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -30,7 +31,8 @@ router.post('/register', (req, res) => {
 
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Size
@@ -63,6 +65,13 @@ router.post('/register', (req, res) => {
 // @desc    Login a user / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -71,7 +80,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       //Check for user
       if (!user) {
-        return res.status(404).json({ email: 'User not found' });
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
       // Check password with bcrypt
       bcrypt.compare(password, user.password).then(isMatch => {
@@ -93,7 +103,8 @@ router.post('/login', (req, res) => {
             }
           );
         } else {
-          res.status(400).json({ password: 'Password incorrect' });
+          errors.password = 'Password incorrect';
+          res.status(400).json(errors);
         }
       });
     })
