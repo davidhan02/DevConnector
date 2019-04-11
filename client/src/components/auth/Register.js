0105a60f7ca383797425ba09965d5c7190207c08
David Han <davidhan.5.25@gmail.com>
2019-04-10 18:53:06 -0700

    started to add redux actions and reducers to register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions';
 
 class Register extends Component {
   constructor() {
@@ -24,18 +26,23 @@ class Register extends Component {
     // Assign variables to new User
     const newUser = { name, email, password, password2 };
 
+    this.props.registerUser(newUser);
+
     // Post with axios
-    axios // No need for localhost:5000 because of proxy value in package.json
-      .post('api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }));
+    // axios // No need for localhost:5000 because of proxy value in package.json
+    //   .post('api/users/register', newUser)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => this.setState({ errors: err.response.data }));
   };
 
   render() {
     const { errors } = this.state;
 
+    const { user } = this.props.auth;
+
     return (
       <div className="register">
+        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -126,4 +133,11 @@ class Register extends Component {
   }
 }
 
-export default Register;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(Register);
