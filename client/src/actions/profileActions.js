6a3f1359691d855b355f05596896e7c2fb2a0ed2
David Han <davidhan.5.25@gmail.com>
2019-04-11 16:59:46 -0700

    Added a delete account functionality to Profile Actions Component

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,12 +1,14 @@
 // Profile Actions
 
 import axios from 'axios';
+import { logoutUser } from './authActions';
 
 import {
   GET_PROFILE,
   PROFILE_LOADING,
   GET_ERRORS,
-  CLEAR_CURRENT_PROFILE
+  CLEAR_CURRENT_PROFILE,
+  SET_CURRENT_USER
 } from './types';
 
 // Get current profile
@@ -54,4 +56,20 @@ export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE
   };
+};
+
+// Delete account & profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This cannot be undone!')) {
+    dispatch(clearCurrentProfile());
+    axios
+      .delete('/api/profile')
+      .then(res => dispatch(logoutUser()))
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
