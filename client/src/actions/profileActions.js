b19d71b01b73310d862f577829b74ba99ba25c65
David Han <davidhan.5.25@gmail.com>
2019-04-12 13:39:55 -0700

    Create Profile display by handle redux and started Profile subcomponents

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -31,6 +31,26 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Get a profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  // Axios get call
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 // Create a profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
