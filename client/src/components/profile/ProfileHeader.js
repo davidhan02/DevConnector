cc41cad28bc8c40235f0a4f5a33101a37473f450
David Han <davidhan.5.25@gmail.com>
2019-04-12 14:33:14 -0700

    Finished Profile Header with links

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -48,15 +48,46 @@ class ProfileHeader extends Component {
                     <i className="fab fa-twitter fa-2x" />
                   </a>
                 )}
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-facebook fa-2x" />
-                </a>
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-linkedin fa-2x" />
-                </a>
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-instagram fa-2x" />
-                </a>
+                {isEmpty(profile.social && profile.social.facebook) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.facebook}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <i className="fab fa-facebook fa-2x" />
+                  </a>
+                )}
+                {isEmpty(profile.social && profile.social.linkedin) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.linkedin}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <i className="fab fa-linkedin fa-2x" />
+                  </a>
+                )}
+                {isEmpty(profile.social && profile.social.youtube) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.youtube}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <i className="fab fa-youtube fa-2x" />
+                  </a>
+                )}
+                {isEmpty(profile.social && profile.social.instagram) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.instagram}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <i className="fab fa-instagram fa-2x" />
+                  </a>
+                )}
               </p>
             </div>
           </div>
